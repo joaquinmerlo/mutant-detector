9de61d1460640e9d13ab7238a4b76f07457aba52
Joaquin <jmerloperfumo@gmail.com>
2019-05-23 16:42:25 -0300

    Arreglo de error de conexion

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -1,19 +1,37 @@
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
+const db_config = {
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'humans'
-});
+}
 
-mysqlConnection.connect(function (err) {
-    if (err) {
-        console.log(err);
-        return
-    } else {
-        console.log('DB is connected');
-    }
-});
+let mysqlConnection;
+
+function dbConnection() {
+
+    mysqlConnection = mysql.createConnection(db_config);
+
+    mysqlConnection.connect(function (err) {
+        if (err) {
+            setTimeout(dbConnection, 2000);
+            return
+        } else {
+            console.log('DB is connected');
+        }
+    });
+
+    mysqlConnection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            dbConnection();
+        } else {
+            throw err;
+        }
+    });
+}
+
+dbConnection();
 
 module.exports = mysqlConnection;
